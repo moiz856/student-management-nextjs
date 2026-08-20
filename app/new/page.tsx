5940b476200354542942e0222c08a2058c1da7f3
moiz856 <abdulmoiz950081@gmail.com>
2026-08-20 14:50:30 +0500

    Create page.tsx

---
+++ app/new/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import StudentForm from "@/components/StudentForm";
+import { createStudent } from "@/services/studentService";
+import { Student } from "@/models/Student";
+
+export default function NewStudentPage() {
+  const router = useRouter();
+
+  const saveStudent = async (student: Student) => {
+    try {
+      await createStudent(student);
+
+      alert("Student Added Successfully");
+
+      router.push("/students");
+    } catch (error) {
+      console.error(error);
+      alert("Error while adding student");
+    }
+  };
+
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-5">
+        Add Student
+      </h1>
+
+      <StudentForm onSubmit={saveStudent} />
+    </div>
+  );
+}
